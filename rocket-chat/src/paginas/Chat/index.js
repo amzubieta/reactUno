1b2756653bf2431b054cd6c575ce08316af6e7b7
Angelica Zubieta <angelicazubieta@gmail.com>
2019-11-22 09:30:58 -0300

    ultimas actualizaciones

--- rocket-chat/src/paginas/Chat/index.js
+++ rocket-chat/src/paginas/Chat/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import{ getMesagens } from '../../service/base'
+
 
 import './styles.css'
+
+class chat extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      mensagens:''
+    }
+  }
+
+  componentDidMount(){
+    getMesagens()
+    .then(response =>{
+      this
+    })
+    .catch(error=>{
+
+    })
+  }
+
+  render()
+}
 
 function Chat () {
   return (
@@ -10,4 +33,3 @@ function Chat () {
   )
 }
 
-export default Chat
--- rocket-chat/src/paginas/Contato/Formulario/index.js
+++ rocket-chat/src/paginas/Contato/Formulario/index.js
@@ -1,76 +1,124 @@
 import React from 'react'
-import
+import Grupo from './componentes/Grupo'
+import Botao from './componentes/Botao'
 
-    class Formulario extends React.Component {
-        constructor(props) {
-            super(props)
-            this.state = {
-                nome: {
-                    valor: '',
-                    erro: ''
-                }
-            email: {
-                    valor: '',
-                    erro: ''
-                }
-            pais: {
-                    valor: '',
-                    erro: ''
-                }
-            mensagem: {
-                    valor: '',
-                    erro: ''
-                }
-            }
-        }
+// props = {
+//   mudaConteudo: this.handleMudaConteudo
+// }
+class Formulario extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nome: {
+        valor: '',
+        erro: ''
+      },
+      email: {
+        valor: '',
+        erro: ''
+      },
+      pais: {
+        valor: '',
+        erro: ''
+      },
+      mensagem: {
+        valor: '',
+        erro: ''
+      }
+    }
+  }
 
-        handleChange = (nomeDoCampo, valorDoCampo, erroDoCampo = '') => {
-            this.setState({
-                [nomeDoCampo]: {
-                    valor: valorDoCampo,
-                    erro: erroDoCampo
-                }
-            })
+  handleChange = (nomeDoCampo, valorDoCampo, erroDoCampo = '') => {
+    this.setState({
+      [nomeDoCampo]: {
+        valor: valorDoCampo,
+        erro: erroDoCampo
+      }
+    })
+  }
 
-        }
+  estaDesabilitado = () => {
+    return (
+      !this.state.nome.valor ||
+      this.state.nome.erro ||
+      !this.state.email.valor ||
+      this.state.email.erro ||
+      !this.state.pais.valor ||
+      this.state.pais.erro
+    )
+  }
 
-        render() {
-            return (
-                <div className='pagina'>
-                    <h2>Entre em contato conosco!</h2>
-                    <form className='formulario'>
-                        <Grupo erro={this.state.nome.erro}>
-                            <Grupo.Legenda htmlFor='nome' nome
-                    </Grupo>
-                </form>
-            </div>
-                    )
-                }
-            }
-            
-            export defaul Formulario
-            
-            
-// <CaixaTexto
-                        //                         name='nome'
-                        //                         placeholder='Digite seu nome:'
-                        //                         mudaEstado={this.handleChange}
-                        //                     />
-                        //                     <CaixaTexto
-                        //                         name='email'
-                        //                         placeholder='Digite seu email:'
-                        //                         mudaEstado={this.handleChange}
-                        //                     />
-                        //                     <CaixaTexto
-                        //                         name='pais'
-                        //                         placeholder='Digite seu país:'
-                        //                         mudaEstado={this.handleChange}
-                        //                     />
-                        //                     <CaixaTexto
-                        //                         name='mensagem'
-                        //                         placeholder='Digite sua mesagem:'
-                        //                         mudaEstado={this.handleChange}
-                        //                     />
-                        import Grupo from './Grupo'
-                    import Grupo from './Grupo'
-        
+  handleSubmit = () => {
+    const novoContato = {
+      nome: this.state.nome.valor,
+      email: this.state.email.valor,
+      pais: this.state.pais.valor,
+      mensagem: this.state.mensagem.valor
+    }
+    console.log(novoContato, 'enviou')
+  }
+
+  render() {
+    const verificaBotao = this.estaDesabilitado()
+    //true ou false
+    return (
+      <div className='pagina'>
+        <h2>Entre em contato conosco!</h2>
+        <form className='formulario' id='form1' onSubmit={this.handleSubmit}>
+          <Grupo erro={this.state.nome.erro} >
+            <Grupo.Legenda htmlFor='nome'> Nome Completo: </Grupo.Legenda>
+            <Grupo.CaixaTexto
+               name='nome'
+               placeholder='Digite seu nome'
+               mudaEstado={this.handleChange}
+               minLength={10}
+               required
+               type='text'
+            />
+          </Grupo>
+          <Grupo erro={this.state.email.erro} >
+            <Grupo.Legenda htmlFor='email'> Email: </Grupo.Legenda>
+            <Grupo.CaixaTexto
+               name='email'
+               placeholder='Digite seu email'
+               mudaEstado={this.handleChange}
+               required
+               type='email'
+            />
+          </Grupo>
+          <Grupo erro={this.state.pais.erro} >
+            <Grupo.Legenda htmlFor='pais'> País: </Grupo.Legenda>
+            <Grupo.CaixaTexto
+               name='pais'
+               placeholder='Digite seu país'
+               mudaEstado={this.handleChange}
+               required
+               type='text'
+            />
+          </Grupo>
+          <Grupo erro={this.state.mensagem.erro} >
+            <Grupo.Legenda htmlFor='mensagem'> Mensagem: </Grupo.Legenda>
+            <Grupo.CaixaTexto
+               name='mensagem'
+               placeholder='Digite sua mensagem'
+               mudaEstado={this.handleChange}
+               type='text'
+            />
+          </Grupo>
+          <Botao
+            desabilitado={verificaBotao}
+            mudaConteudo={this.props.mudaConteudo}
+            onSubmit={this.handleSubmit}
+            pagina='sucesso'
+            type='submit'
+          >
+            Enviar
+          </Botao>
+
+      </form>
+    </div>
+    )
+  }
+}
+
+export default Formulario
